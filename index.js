0014b7abb2f4d70b81ec008663135da4ca55ad2a
Dev-Olena <alenagruda@gmail.com>
2024-06-24 14:57:22 +0300

    Add description to slides

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 const slides = [{
     src: 'https://images.pexels.com/photos/462162/pexels-photo-462162.jpeg?auto=compress&cs=tinysrgb&w=600',
-    alt: 'landscape1'
+    alt: 'landscape1',
+    description: 'Lorem ipsum dolor sit amet 1'
 }, {
     src: 'https://images.pexels.com/photos/158063/bellingrath-gardens-alabama-landscape-scenic-158063.jpeg?auto=compress&cs=tinysrgb&w=600',
-    alt: 'landscape2'
+    alt: 'landscape2',
+    description: 'Lorem ipsum dolor sit amet 2'
 }, {
     src: 'https://images.pexels.com/photos/131723/pexels-photo-131723.jpeg?auto=compress&cs=tinysrgb&w=600',
-    alt: 'landscape3'
+    alt: 'landscape3',
+    description: 'Lorem ipsum dolor sit amet 3'
 }, {
     src: 'https://images.pexels.com/photos/1462892/pexels-photo-1462892.jpeg?auto=compress&cs=tinysrgb&w=600',
-    alt: 'landscape4'
+    alt: 'landscape4',
+    description: 'Lorem ipsum dolor sit amet 4'
 }, {
     src: 'https://images.pexels.com/photos/547119/pexels-photo-547119.jpeg?auto=compress&cs=tinysrgb&w=600',
-    alt: 'landscape5'
+    alt: 'landscape5',
+    description: 'Lorem ipsum dolor sit amet 5'
 }, {
     src: 'https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?auto=compress&cs=tinysrgb&w=600',
-    alt: 'landscape6'
+    alt: 'landscape6',
+    description: 'Lorem ipsum dolor sit amet 6'
 }]
 
 const [prevBtn, nextBtn] = document.querySelectorAll('button');
 const sliderImg = document.querySelector('.sliderWrapper img');
+const slideDescription = document.querySelector('.sliderWrapper p');
 
 
 const slider = new Slider(slides);
 
 function updateSlider() {
     sliderImg.src = slider.currentSlide.src;
-    sliderImg.alt = slider.currentSlide.alt
+    sliderImg.alt = slider.currentSlide.alt;
+    console.log(slider.currentSlide);
+    slideDescription.textContent = slider.currentSlide.description;
 }
 
 function createBtnHandler(direction) {
